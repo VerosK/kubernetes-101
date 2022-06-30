cbf24e7501d370d431c77e2968c9c5440209c82d
Věroš Kaplan <VerosK@users.noreply.github.com>
2022-06-30 09:38:48 +0200

    chore: add comments to helm lab

--- 03-helm/prepare-kube-ops-view.sh
+++ 03-helm/prepare-kube-ops-view.sh
@@ -3,15 +3,24 @@
 set -e
 set -x
 
+
+# prepare namespaces
 kubectl get ns kube-ops-view || kubectl create ns kube-ops-view
 
+# Add helm chart repo
 helm repo add k8s-at-home https://k8s-at-home.com/charts/
-
 helm repo update k8s-at-home
 
-helm install kube-ops-view k8s-at-home/kube-ops-view --namespace kube-ops-view
+# install the chart
+helm install kube-ops-view k8s-at-home/kube-ops-view --values values.yml --namespace kube-ops-view
 
+#or render it
+helm template kube-ops-view k8s-at-home/kube-ops-view --values values.yml --namespace kube-ops-view
+
+
+# upgrade
 #helm upgrade --install kube-ops-view k8s-at-home/kube-ops-view
 
+# inspect
 #helm pull k8s-at-home/kube-ops-view --untar
 
