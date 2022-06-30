3b4dbac90589bf22b013826a7433b305a4e4cbf8
Věroš Kaplan <VerosK@users.noreply.github.com>
2022-06-30 09:36:25 +0200

    fix: typo in namespace

--- 03-helm/prepare-kube-ops-view.sh
+++ 03-helm/prepare-kube-ops-view.sh
@@ -3,13 +3,13 @@
 set -e
 set -x
 
-kubectl get ns metrics-server || kubectl create ns metrics-server
+kubectl get ns kube-ops-view || kubectl create ns kube-ops-view
 
 helm repo add k8s-at-home https://k8s-at-home.com/charts/
 
 helm repo update k8s-at-home
 
-helm install kube-ops-view k8s-at-home/kube-ops-view
+helm install kube-ops-view k8s-at-home/kube-ops-view --namespace kube-ops-view
 
 #helm upgrade --install kube-ops-view k8s-at-home/kube-ops-view
 
